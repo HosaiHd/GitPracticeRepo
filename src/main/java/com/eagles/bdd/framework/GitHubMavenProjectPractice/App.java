3e4ec315335a12acabd67384b69a181044791b0d
Hosaihd <hosai.dawlatzai@gmail.com>
2021-07-27 15:54:30 -0600

    This is practice code

---
+++ src/main/java/com/eagles/bdd/framework/GitHubMavenProjectPractice/App.java
@@ -0,0 +1,13 @@
+package com.eagles.bdd.framework.GitHubMavenProjectPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
